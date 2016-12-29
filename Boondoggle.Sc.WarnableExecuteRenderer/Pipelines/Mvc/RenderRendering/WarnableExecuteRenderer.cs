41fadf8fad8efe07736f805e2e04915cc8c3a486
Filip Verswijver <filip.verswijver@boondoggle.eu>
2016-12-29 09:36:35 +0100

    - Initial commit

---
+++ Boondoggle.Sc.WarnableExecuteRenderer/Pipelines/Mvc/RenderRendering/WarnableExecuteRenderer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using Sitecore.Mvc.Presentation;
+
+namespace Boondoggle.Sc.WarnableExecuteRenderer.Pipelines.Mvc.RenderRendering
+{
+    public class WarnableExecuteRenderer : ExecuteRenderer
+    {
+        private readonly string _renderingModelErrorMessage;
+        private readonly string _errorHtmlTemplate;
+        private readonly bool _renderErrorOnlyWhileEditing;
+
+        public WarnableExecuteRenderer() : base()
+        {
+            _renderingModelErrorMessage = Sitecore.Configuration.Settings.GetSetting("WarnableExecuteRenderer.ErrorMessage",
+                "The model item passed into the dictionary is of type 'Sitecore.Mvc.Presentation.RenderingModel'");
+            _errorHtmlTemplate = Sitecore.Configuration.Settings.GetSetting("WarnableExecuteRenderer.HtmlTemplate",
+                "<div style=\"padding: 10px; background-color: red;\"><h1>An error occured while trying to render this component.</h1><ul><li>Placeholder: '{0}'</li><li>Rendering: '{1}'</li><li>Datasource: '{2}'</li></ul></div>");
+            _renderErrorOnlyWhileEditing =
+                Sitecore.Configuration.Settings.GetBoolSetting("WarnableExecuteRenderer.ErrorOnlyWhileEditing", false);
+        }
+
+        #region Overrides of ExecuteRenderer
+
+        protected override bool Render(Renderer renderer, TextWriter writer, RenderRenderingArgs args)
+        {
+            try
+            {
+                return base.Render(renderer, writer, args);
+            }
+            catch (InvalidOperationException invalidOperationException)
+            {
+                if (!IsRenderingModelError(invalidOperationException)) throw;
+                if (args.Rendering == null || args.Rendering.Renderer == null) throw;
+
+                var renderingString = "";
+                if (args.Rendering.Renderer is ViewRenderer)
+                {
+                    var rendering = ((ViewRenderer)args.Rendering.Renderer);
+                    renderingString = rendering.ViewPath;
+                }
+                else if (args.Rendering.Renderer is ControllerRenderer)
+                {
+                    var rendering = ((ControllerRenderer)args.Rendering.Renderer);
+                    renderingString = rendering.ControllerName + "." + rendering.ActionName;
+                }
+
+                var errorMessage =
+                    string.Format(
+                        "Unable to render rendering due to missing datasource. Placeholder: '{0}', Rendering: '{1}', Datasource: '{2}'",
+                        args.Rendering.Placeholder, renderingString, args.Rendering.DataSource);
+
+                Log.Error(errorMessage, invalidOperationException, GetType());
+
+                if ((_renderErrorOnlyWhileEditing && Sitecore.Context.PageMode.IsExperienceEditorEditing) ||
+                    !_renderErrorOnlyWhileEditing)
+                {
+                    writer.WriteLine(_errorHtmlTemplate, args.Rendering.Placeholder, renderingString,
+                        args.Rendering.DataSource);
+                }
+                else
+                {
+                    throw new Exception(errorMessage, invalidOperationException);
+                }
+
+                return true;
+
+            }
+        }
+
+        #endregion
+
+        private bool IsRenderingModelError(InvalidOperationException exception)
+        {
+            if (exception == null) return false;
+            if (exception.InnerException == null) return false;
+            if (string.IsNullOrWhiteSpace(exception.InnerException.Message)) return false;
+
+            return exception.InnerException.Message.StartsWith(_renderingModelErrorMessage);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Boondoggle.Sc.WarnableExecuteRenderer
-Small code for a wrapper around the Sitecore MVC ExecuteRenderer
+# Boondoggle Warnable ExecuteRenderer
+With support from [![Provided by Boondoggle](http://res.cloudinary.com/dr8gt19s9/image/upload/v1473586972/bd_logo_ej5jjd.gif)](http://www.boondoggle.eu)
+
+This allows you to show (and log) a proper message when an error of type `The model item passed into the dictionary is of type 'Sitecore.Mvc.Presentation.RenderingModel', but this dictionary requires a model item of type 'MyNamespace.Models.MyGlassModel'` occurs.
+
+Source: [http://blog.verswijver.com/proper-renderingmodel-error/](http://blog.verswijver.com/proper-renderingmodel-error/)
+
+## Information
+* Nuget is used for any dependencies to Sitecore
+* As minimal outside dependencies are used (no ORM mapper for example)
+* Enjoy!
